34a47a5a0559f2c5350cc67f67e9d98a6b116412
Fredrik Svensson <fs222id@student.lnu.se>
2024-03-02 10:02:14 +0100

    Add linting configuration and fix linting errors

--- test/server.test.js
+++ test/server.test.js
@@ -1,7 +1,7 @@
-import server from '../src/server';
+import server from '../src/server'
 
 describe('Server configuration', () => {
   it('should import express', () => {
-    expect(server.app).toBeDefined();
+    expect(server.app).toBeDefined()
   })
-});
+})
