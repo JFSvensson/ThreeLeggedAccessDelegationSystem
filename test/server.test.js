2a56865f8ba56c9e8d8a79840e23c92519c306e4
Fredrik Svensson <fs222id@student.lnu.se>
2024-03-02 10:14:29 +0100

    Add test for starting HTTP server

--- test/server.test.js
+++ test/server.test.js
@@ -1,7 +1,13 @@
 import server from '../src/server'
+import request from 'supertest'
 
 describe('Server configuration', () => {
   it('should import express', () => {
     expect(server.app).toBeDefined()
+  })
+
+  it('should start an HTTP server', async () => {
+    const response = await request(server.app).get('/')
+    expect(response.status).toBe(200)
   })
 })
