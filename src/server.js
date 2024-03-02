f7bb6bd130cc2dfc9e6a59cfdc4c82ca600e2e48
Fredrik Svensson <fs222id@student.lnu.se>
2024-03-02 10:06:46 +0100

    Add file header to server.js

--- src/server.js
+++ src/server.js
@@ -1,3 +1,12 @@
+/**
+ * This is the entry point of the application.
+ *
+ * @file server.js is the root file that starts the server.
+ * @author Fredrik Svensson
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+
 import express from 'express'
 const app = express()
 
