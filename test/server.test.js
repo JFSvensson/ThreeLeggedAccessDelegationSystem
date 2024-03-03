da814a6b6371da8e27139ee915146df9f2eac515
Fredrik Svensson <fs222id@student.lnu.se>
2024-03-03 21:40:48 +0100

    Refactor GET / test in server.test.js

--- test/server.test.js
+++ test/server.test.js
@@ -86,13 +86,13 @@ describe('Logger', () => {
   })
 })
 
-// describe('GET /', () => {
-//   it('should render the index view', async () => {
-//     const res = await request(server).get('/')
-//     expect(res.status).toBe(200)
-//     expect(res.text).toContain('Welcome')
-//   })
-// })
+describe('GET /', () => {
+  it('should render the index view', async () => {
+    const response = await request(server.app).get('/')
+    expect(response.status).toBe(200)
+    expect(response.text).toContain('Welcome')
+  })
+})
 
 afterAll(() => {
   server.server.close()
