545960e1dfb4857a2751926abf33a91bf615fe37
Fredrik Svensson <fs222id@student.lnu.se>
2024-03-03 21:34:31 +0100

    Add transform-import-meta plugin to Babel configuration

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,3 +1,4 @@
 module.exports = {
-  presets: [['@babel/preset-env', { targets: { node: 'current' }, modules: 'commonjs' }]]
+  presets: [['@babel/preset-env', { targets: { node: 'current' }, modules: 'commonjs' }]],
+  plugins: ['transform-import-meta']
 }
